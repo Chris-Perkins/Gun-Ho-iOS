Chris-Perkins/Gun-Ho-iOS

commit 92f331468271abeb4eecbed6fcfd3635526e8f30

FEATURE: Island/boat/whale collisions set up.

Gun Ho/Game Engine Supporting Files/GameManager.swift
@@ -36,7 +36,17 @@ public class GameManager: NSObject {
     // MARK: Game properties
     
     // The root node for use in spawning and finding the worldScene node
-    public var rootNode: SCNNode?
+    public var rootNode: SCNNode? {
+        didSet {
+            guard let islandEnvironment = island.childNode(withName: "environment",
+                                                           recursively: false),
+                let islandPhysicsBody = islandEnvironment.physicsBody else {
+                fatalError("Could not get island/it's physics body!")
+            }
+            islandPhysicsBody.categoryBitMask  = CollisionType.island
+            islandPhysicsBody.collisionBitMask = CollisionType.island
+        }
+    }
     
     // Stores all game objects so we can check logic for each frame
     public var gameObjects = [GameObject]()
@@ -165,7 +175,6 @@ extension GameManager {
         }
         gameObjects.removeAll()
         
-        print("GAME OVER U LOSE")
         Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { (timer) in
             self.performGameStartSequence()
         }
@@ -221,6 +230,19 @@ extension GameManager {
 
 extension GameManager: SCNPhysicsContactDelegate {
     public func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
-        print(contact)
+        let collisionMask = contact.nodeA.physicsBody!.collisionBitMask | contact.nodeB.physicsBody!.collisionBitMask
+        
+        switch collisionMask {
+        case CollisionType.boat | CollisionType.island:
+            performGameOverSequence()
+        case CollisionType.boat | CollisionType.boat:
+            contact.nodeA.boatParent?.destroy()
+            contact.nodeB.boatParent?.destroy()
+        default:
+            // Unhandled collision, but not necessarily an error.
+            break
+        }
+        print(collisionMask)
+        //performGameOverSequence()
     }
 }

Gun Ho/Supporting Files/SCNNodeExtension.swift
@@ -10,6 +10,6 @@ import SceneKit
 
 extension SCNNode {
     var boatParent: Boat? {
-        return parent?.parent as? Boat
+        return parent as? Boat
     }
 }

Gun Ho.xcodeproj/project.pbxproj
@@ -15,6 +15,7 @@
 		225815FC20328F3D00F546DD /* VikingBoat.swift in Sources */ = {isa = PBXBuildFile; fileRef = 225815FB20328F3D00F546DD /* VikingBoat.swift */; };
 		2261C1092037D805003E580C /* SceneViewExtensions.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2261C1082037D805003E580C /* SceneViewExtensions.swift */; };
 		2261C10B2037D960003E580C /* SCNTransactionExtensions.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2261C10A2037D960003E580C /* SCNTransactionExtensions.swift */; };
+		226D9D0C205EE47A002F05DD /* CollisionMasksForGame.swift in Sources */ = {isa = PBXBuildFile; fileRef = 226D9D0B205EE47A002F05DD /* CollisionMasksForGame.swift */; };
 		227F984C2034CA7D007FF294 /* SCNNodeExtension.swift in Sources */ = {isa = PBXBuildFile; fileRef = 227F984B2034CA7D007FF294 /* SCNNodeExtension.swift */; };
 		228AB9352030D28500FEC484 /* Boat.swift in Sources */ = {isa = PBXBuildFile; fileRef = 228AB9342030D28500FEC484 /* Boat.swift */; };
 		22A3F5E62045D5AB00D89358 /* BoatSpawner.swift in Sources */ = {isa = PBXBuildFile; fileRef = 22A3F5E52045D5AB00D89358 /* BoatSpawner.swift */; };
@@ -38,6 +39,7 @@
 		225815FB20328F3D00F546DD /* VikingBoat.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = VikingBoat.swift; sourceTree = "<group>"; };
 		2261C1082037D805003E580C /* SceneViewExtensions.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SceneViewExtensions.swift; sourceTree = "<group>"; };
 		2261C10A2037D960003E580C /* SCNTransactionExtensions.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SCNTransactionExtensions.swift; sourceTree = "<group>"; };
+		226D9D0B205EE47A002F05DD /* CollisionMasksForGame.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = CollisionMasksForGame.swift; sourceTree = "<group>"; };
 		227F984B2034CA7D007FF294 /* SCNNodeExtension.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SCNNodeExtension.swift; sourceTree = "<group>"; };
 		228AB9342030D28500FEC484 /* Boat.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = Boat.swift; sourceTree = "<group>"; };
 		22A3F5E52045D5AB00D89358 /* BoatSpawner.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BoatSpawner.swift; sourceTree = "<group>"; };
@@ -70,6 +72,7 @@
 				222BE22D203605D40036E867 /* GameManager.swift */,
 				22A3F5E52045D5AB00D89358 /* BoatSpawner.swift */,
 				222BE22C203605D40036E867 /* GameObject.swift */,
+				226D9D0B205EE47A002F05DD /* CollisionMasksForGame.swift */,
 			);
 			path = "Game Engine Supporting Files";
 			sourceTree = "<group>";
@@ -215,6 +218,7 @@
 				227F984C2034CA7D007FF294 /* SCNNodeExtension.swift in Sources */,
 				22A3F5E62045D5AB00D89358 /* BoatSpawner.swift in Sources */,
 				2245B61020336A8B00F1369B /* GameViewController.swift in Sources */,
+				226D9D0C205EE47A002F05DD /* CollisionMasksForGame.swift in Sources */,
 				35BE87A72036620D004EA2CF /* SailBoat.swift in Sources */,
 				222BE22E203605D40036E867 /* GameObject.swift in Sources */,
 				225815FA20328EA900F546DD /* MediumBoat.swift in Sources */,

Gun Ho/Boats/Boat.swift
@@ -33,7 +33,7 @@ class Boat: GameObject {
     }
     
     // MARK: - Lifecycle
-    init(maxHealth: Int, floatHeight: Float, points: Int, speed: Int) {
+    init(maxHealth: Int, floatHeight: Float, points: Int, speed: Int, withNode node: SCNNode) {
         self.maxHealth   = maxHealth
         self.floatHeight = floatHeight
         self.pointValue  = points
@@ -42,6 +42,14 @@ class Boat: GameObject {
         health = maxHealth
         
         super.init()
+        
+        addChildNode(node)
+        
+        guard let boatPhysicsBody = node.physicsBody else {
+            fatalError("Could not get boat physics body! Does it exist?")
+        }
+        boatPhysicsBody.categoryBitMask  = CollisionType.boat
+        boatPhysicsBody.collisionBitMask = CollisionType.boat
     }
     
     required override init() {

Gun Ho/Boats/MediumBoat.swift
@@ -14,17 +14,17 @@ final class MediumBoat: Boat {
     override class var pointsCount: Int { return  3 }
     
     required init() {
-        super.init(maxHealth: 3,
-                   floatHeight: -0.05,
-                   points: MediumBoat.pointsCount,
-                   speed: 3)
-        
         guard let scene = SCNScene(named: "art.scnassets/boat-medium.scn"),
             let boatNode = scene.rootNode.childNode(withName: "boat",
                                                     recursively: true) else {
                                                         fatalError("Could not find medium boat")
         }
-        addChildNode(boatNode)
+        
+        super.init(maxHealth: 3,
+                   floatHeight: -0.05,
+                   points: MediumBoat.pointsCount,
+                   speed: 3,
+                   withNode: boatNode)
     }
     
     required init?(coder aDecoder: NSCoder) {

Gun Ho/Boats/SailBoat.swift
@@ -14,17 +14,17 @@ final class SailBoat: Boat {
     override class var pointsCount: Int { return  7 }
     
     required init() {
-        super.init(maxHealth: 7,
-                   floatHeight: -0.175,
-                   points: SailBoat.pointsCount,
-                   speed: 5)
-        
         guard let scene = SCNScene(named: "art.scnassets/boat-sail.scn"),
             let boatNode = scene.rootNode.childNode(withName: "boat",
                                                     recursively: true) else {
                                                         fatalError("Could not find sail boat")
         }
-        addChildNode(boatNode)
+        
+        super.init(maxHealth: 7,
+                   floatHeight: -0.175,
+                   points: SailBoat.pointsCount,
+                   speed: 5,
+                   withNode: boatNode)
     }
     
     required init?(coder aDecoder: NSCoder) {

Gun Ho/Boats/SmallBoat.swift
@@ -14,17 +14,17 @@ final class SmallBoat: Boat {
     override class var pointsCount: Int { return  1 }
     
     required init() {
-        super.init(maxHealth: 1,
-                   floatHeight: -0.05,
-                   points: SmallBoat.pointsCount,
-                   speed: 5)
-        
         guard let scene = SCNScene(named: "art.scnassets/boat-small.scn"),
             let boatNode = scene.rootNode.childNode(withName: "boat",
                                                     recursively: true) else {
                                                         fatalError("Could not find small boat")
         }
-        addChildNode(boatNode)
+        
+        super.init(maxHealth: 1,
+                   floatHeight: -0.05,
+                   points: SmallBoat.pointsCount,
+                   speed: 5,
+                   withNode: boatNode)
     }
     
     required init?(coder aDecoder: NSCoder) {

commit af381e38fde1ab6262a360855daae8c53d00c878

BUGFIX: boat tapping broken introduced in 92f331468271abeb4eecbed6fcfd3635526e8f30

Gun Ho/Game Engine Supporting Files/GameManager.swift
@@ -236,8 +236,10 @@ extension GameManager: SCNPhysicsContactDelegate {
         case CollisionType.boat | CollisionType.island:
             performGameOverSequence()
         case CollisionType.boat | CollisionType.boat:
-            contact.nodeA.boatParent?.destroy()
-            contact.nodeB.boatParent?.destroy()
+            /* The 'parent' is here since the physicsbody of the boat
+                is attached to the immediate child of the boat object */
+            (contact.nodeA.parent as? Boat)?.destroy()
+            (contact.nodeA.parent as? Boat)?.destroy()
         default:
             // Unhandled collision, but not necessarily an error.
             break

Gun Ho/Supporting Files/SCNNodeExtension.swift
@@ -10,6 +10,6 @@ import SceneKit
 
 extension SCNNode {
     var boatParent: Boat? {
-        return parent as? Boat
+        return parent?.parent as? Boat
     }
 }