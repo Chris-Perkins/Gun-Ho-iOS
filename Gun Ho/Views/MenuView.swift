bbf34e2ef3d5ec7b0585f5bd8e4a481f384a6138
Alex <JaedaBeverly@Joelles-MacBook-Air.local>
2018-03-28 13:56:48 -0400

    Add MenuView to the Views folder
    Create Menu for Tap to Play, Game Rules, and Info

---
+++ Gun Ho/Views/MenuView.swift
@@ -0,0 +1,15 @@
+//
+//  MenuView.swift
+//  Gun Ho
+//
+//  Created by Joelle Beverly on 3/28/18.
+//  Copyright © 2018 Christopher Perkins. All rights reserved.
+//
+
+import UIKit
+
+class MenuView: UIView {
+    
+    // MARK: View Properties
+    
+}
