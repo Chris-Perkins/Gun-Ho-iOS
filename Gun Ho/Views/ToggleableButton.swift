4e765849219815b881b04e2d6aab4de3c1cb7331
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2018-04-06 12:04:12 -0400

    NEW: Create ToggleableButton class

---
+++ Gun Ho/Views/ToggleableButton.swift
@@ -0,0 +1,100 @@
+//
+//  ToggleableButton.swift
+//  Gun Ho
+//
+//  Created by Christopher Perkins on 4/6/18.
+//  Copyright © 2018 Christopher Perkins. All rights reserved.
+//
+// Adapted and re-used from my old "Lifting Buddy" project
+
+import UIKit
+
+public class ToggleableButton: UIButton {
+    
+    // MARK: View properties
+    
+    // Whether or not the button is toggled (default false)
+    @IBInspectable public var isToggled: Bool = false {
+        didSet {
+            backgroundColor = isToggled ? toggleViewColor : defaultViewColor
+            setTitleColor(isToggled ? toggleTextColor : defaultTextColor, for: .normal)
+            
+            // If we toggled to a state that has non-nil text
+            if (!isToggled && defaultText != nil) || (isToggled && toggleText != nil) {
+                setTitle(isToggled ? toggleText : defaultText, for: .normal)
+            }
+        }
+    }
+    
+    // Toggle text (string)
+    @IBInspectable public var toggleText: String? {
+        didSet {
+            if isToggled {
+                setTitle(toggleText, for: .normal)
+            }
+        }
+    }
+    // The color when toggled
+    @IBInspectable public var toggleViewColor: UIColor? {
+        didSet {
+            if isToggled {
+                backgroundColor = toggleViewColor
+            }
+        }
+    }
+    // The text color when toggled
+    @IBInspectable public var toggleTextColor: UIColor? {
+        didSet {
+            if isToggled {
+                setTitleColor(toggleTextColor, for: .normal)
+            }
+        }
+    }
+    
+    // Sets default text (untoggled)
+    @IBInspectable public var defaultText: String? {
+        didSet {
+            if !isToggled {
+                setTitle(defaultText, for: .normal)
+            }
+        }
+    }
+    // The default color (untoggled)
+    @IBInspectable public var defaultViewColor: UIColor? {
+        didSet {
+            if !isToggled {
+                backgroundColor = defaultViewColor
+            }
+        }
+    }
+    // The default text color (untoggled)
+    @IBInspectable public var defaultTextColor: UIColor? {
+        didSet {
+            if !isToggled {
+                setTitleColor(defaultTextColor, for: .normal)
+            }
+        }
+    }
+    
+    // MARK: Inits
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addTarget(self, action: #selector(buttonPress(sender:)), for: .touchUpInside)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        addTarget(self, action: #selector(buttonPress(sender:)), for: .touchUpInside)
+    }
+    
+    // MARK: Event functions
+    
+    @objc public func buttonPress(sender: UIButton) {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.isToggled = !self.isToggled
+        })
+    }
+}
