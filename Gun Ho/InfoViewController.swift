bb319c3a0d4a8320f507e07612d0b65c58eccbc0
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2018-04-09 22:18:58 -0400

    NEW: Work on InfoViewController. Still need to give the view a description.

--- Gun Ho/InfoViewController.swift
+++ Gun Ho/InfoViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class InfoViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var emailUsButton: UIButton!
+    @IBOutlet weak var githubButton: UIButton!
+    
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func buttonPress(_ sender: UIButton) {
+        switch sender {
+        case githubButton:
+            if let url = URL(string: "https:////github.com//Chris-Perkins//Gun-Ho-iOS") {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        case emailUsButton:
+            if let url = URL(string: "mailto:chris@chrisperkins.me") {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        default:
+            fatalError("Unhandled button press in InfoViewController")
+        }
     }
 }
