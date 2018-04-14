59083e95ffbf90857b013aa2c5811c83344ba3e6
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2018-04-14 17:26:13 -0400

    POLISH: Polish GameOverViewController

--- Gun Ho/View Controllers/GameOverViewController.swift
+++ Gun Ho/View Controllers/GameOverViewController.swift
@@ -13,15 +13,53 @@ import UIKit
 
 public class GameOverViewController: ScoreDisplayViewController {
     
-    @IBOutlet weak var closeButton: UIButton!
+    // MARK: Properties
     
+    // The button we press to close the view
+    @IBOutlet weak var closeButton: UIButton!
+    // The labels which display the top scores
+    @IBOutlet var scoreLabels: [UILabel]!
+    
+    // MARK: Life-cycle
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setScoreLabelsTextToTopScores()
+    }
+    
+    // MARK: Events
     
     @IBAction func buttonPress(_ sender: UIButton) {
         switch sender {
         case closeButton:
             dismiss(animated: true)
         default:
-            fatalError("Unhandled button press")
+            fatalError("Unhandled button press in GameOverViewController")
+        }
+    }
+    
+    // MARK: Custom functions
+    
+    // Sets the scores for the top labels
+    private func setScoreLabelsTextToTopScores() {
+        // Whether or not we printed " (new!)" by a score we just got.
+        var printedDisplayedScore = false
+        let topScores = Array(previousScoresReference.sorted().reversed())
+        
+        for (index, label) in scoreLabels.enumerated() {
+            if index >= topScores.count {
+                label.text = "--"
+            } else {
+                label.text = "\(topScores[index])"
+                
+                // If this is the score we just got...
+                if topScores[index] == displayScore && !printedDisplayedScore {
+                    label.text = label.text! + NSLocalizedString("GameOver.Labels.New",
+                                                                 comment: "")
+                    printedDisplayedScore = true
+                }
+            }
         }
     }
 }
