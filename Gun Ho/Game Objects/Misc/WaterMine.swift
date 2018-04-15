c353a52996ccfb450bccd94d43a24241104c9a30
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2018-04-15 00:10:47 -0400

    NEW: Particles on watermine detonation

--- Gun Ho/Game Objects/Misc/WaterMine.swift
+++ Gun Ho/Game Objects/Misc/WaterMine.swift
@@ -40,4 +40,35 @@ public class WaterMine: GameObject {
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: Game Object overrides
+    
+    override public func destroy() {
+        // Used to notify the user that the watermine was destroyed
+        attachWatermineDestroyedParticleToWorld()
+        
+        super.destroy()
+    }
+    
+    // MARK: Custom functions
+    
+    // Used to let the user know that the boat was destroyed
+    private func attachWatermineDestroyedParticleToWorld() {
+        // Retrieves the particle system
+        let wmExplosionParticle = SCNParticleSystem(named: "watermine-explosion", inDirectory: nil)!
+        
+        // Adds the particle system to the scene
+        GameManager.shared.scene?.addParticleSystem(wmExplosionParticle,
+                                                    transform: presentation.worldTransform)
+        
+        // Retrieves the time the particle system displays for
+        let totalParticleTime = wmExplosionParticle.particleLifeSpan +
+            wmExplosionParticle.emissionDuration
+        
+        // removes the particle system after this time
+        Timer.scheduledTimer(withTimeInterval: TimeInterval(totalParticleTime), repeats: false) {
+            (timer) in
+            GameManager.shared.scene?.removeParticleSystem(wmExplosionParticle)
+        }
+    }
 }
